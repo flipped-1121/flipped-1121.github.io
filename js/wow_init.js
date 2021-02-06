8fd35a12f69f4ab52056a7ccad8f7c9c1981c965
flipped-1121 <2517421382@qq.com>
2021-02-07 01:49:37 +0800

    Site updated: 2021-02-07 01:49:37

--- js/wow_init.js
+++ js/wow_init.js
@@ -1,2 +1,2 @@
-//初始化函数 wow = new WOW({boxClass:'wow',//当用户滚动时显示隐藏框的类名称 animateClass: 'animate__animated',//触发 CSS 动画的类名称（动画库默认为"animate.css"库） offset: 0,//定义浏览器视口底部与隐藏框顶部之间的距离。//当用户滚动并到达此距离时，将显示隐藏的框。 mobile: true,//在移动设备上打开/关闭wow.js。 live: true//在页面上检查新的 wow.js元素。})
+wow = new WOW({boxClass:'wow',animateClass: 'animate__animated',offset: 0,mobile: true,live: true})
 wow.init();__ESCAPED_SOURCE_END_CLEAN_CSS__
